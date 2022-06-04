a6bccba8d2e6930ca2bb695f42bf9f76971f3e17
Brian <StiiizzyCat@proton.me>
2022-06-03 23:39:46 -0700

    Created themes.js XD
    Themes for AssetTools

---
+++ utl/theme.js
@@ -0,0 +1,99 @@
+const chalk = require('chalk');
+/**
+ * @author: Stiizzy Cat#0001
+ * @description:  Stiizzy Cats Asset Menu Themes
+ * @copyright: (C) Stiizzy Cat 2022
+ **/
+
+function theme(color) {
+    console.clear()
+    if (color === "red") { // devils blood >:]
+        console.log(chalk.redBright("  ▄▄        ██████   ██████ ▓█████▄▄▄█████▓   ▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████    "))
+        console.log(chalk.redBright(" ▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒    "))
+        console.log(chalk.redBright(" ▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███  ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄      "))
+        console.log(chalk.redBright(" ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄░ ▓██▓ ░    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒   "))
+        console.log(chalk.redBright("  ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒ ▒██▒ ░      ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒   "))
+        console.log(chalk.redBright(" ▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░        ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░  "))
+        console.log(chalk.redBright("   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░   ░           ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░   "))
+        console.log(chalk.redBright("   ░   ▒   ░  ░  ░  ░  ░  ░     ░    ░           ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░     "))
+        console.log(chalk.redBright("       ░  ░      ░        ░     ░  ░                        ░ ░      ░ ░      ░  ░      ░     "))
+        console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤV1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+
+    } else if (color === "blue") { // Sky color
+        console.log(chalk.redBright("  ▄▄        ██████   ██████ ▓█████▄▄▄█████▓   ▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████    "))
+        console.log(chalk.redBright(" ▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒    "))
+        console.log(chalk.redBright(" ▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███  ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄      "))
+        console.log(chalk.redBright(" ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄░ ▓██▓ ░    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒   "))
+        console.log(chalk.redBright("  ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒ ▒██▒ ░      ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒   "))
+        console.log(chalk.redBright(" ▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░        ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░  "))
+        console.log(chalk.redBright("   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░   ░           ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░   "))
+        console.log(chalk.redBright("   ░   ▒   ░  ░  ░  ░  ░  ░     ░    ░           ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░     "))
+        console.log(chalk.redBright("       ░  ░      ░        ░     ░  ░                        ░ ░      ░ ░      ░  ░      ░     "))
+        console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤV1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+
+    } else if (color === "purple") { // the drug lovers :3
+        console.log(chalk.hex('#800080')("  ▄▄        ██████   ██████ ▓█████▄▄▄█████▓   ▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████    "))
+        console.log(chalk.hex('#800080')(" ▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒    "))
+        console.log(chalk.hex('#800080')(" ▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███  ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄      "))
+        console.log(chalk.hex('#800080')(" ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄░ ▓██▓ ░    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒   "))
+        console.log(chalk.hex('#800080')("  ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒ ▒██▒ ░      ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒   "))
+        console.log(chalk.hex('#800080')(" ▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░        ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░  "))
+        console.log(chalk.hex('#800080')("   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░   ░           ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░   "))
+        console.log(chalk.hex('#800080')("   ░   ▒   ░  ░  ░  ░  ░  ░     ░    ░           ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░     "))
+        console.log(chalk.hex('#800080')("       ░  ░      ░        ░     ░  ░                        ░ ░      ░ ░      ░  ░      ░     "))
+
+        console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤV1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+
+
+    } else if (color === "usa") { // I live in the usa
+        console.log(chalk.redBright("  ▄▄        ██████   ██████ ▓█████▄▄▄█████▓   ▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████    "))
+        console.log(chalk.redBright(" ▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒    "))
+        console.log(chalk.whiteBright(" ▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███  ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄      "))
+        console.log(chalk.whiteBright(" ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄░ ▓██▓ ░    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒   "))
+        console.log(chalk.redBright("  ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒ ▒██▒ ░      ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒   "))
+        console.log(chalk.redBright(" ▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░        ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░  "))
+        console.log(chalk.whiteBright("   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░   ░           ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░   "))
+        console.log(chalk.whiteBright("   ░   ▒   ░  ░  ░  ░  ░  ░     ░    ░           ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░     "))
+        console.log(chalk.whiteBright("       ░  ░      ░        ░     ░  ░                        ░ ░      ░ ░      ░  ░      ░     "))
+
+        console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤV1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+
+    } else if (color === "ukraine") { // Support Ukraine
+        console.log(chalk.blueBright("  ▄▄        ██████   ██████ ▓█████▄▄▄█████▓   ▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████    "))
+        console.log(chalk.blueBright(" ▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒    "))
+        console.log(chalk.blueBright(" ▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███  ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄      "))
+        console.log(chalk.yellowBright(" ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄░ ▓██▓ ░    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒   "))
+        console.log(chalk.yellowBright("  ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒ ▒██▒ ░      ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒   "))
+        console.log(chalk.yellowBright(" ▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░        ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░  "))
+        console.log(chalk.yellowBright("   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░   ░           ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░   "))
+        console.log(chalk.yellowBright("   ░   ▒   ░  ░  ░  ░  ░  ░     ░    ░           ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░     "))
+        console.log(chalk.yellowBright("       ░  ░      ░        ░     ░  ░                        ░ ░      ░ ░      ░  ░      ░     "))
+        console.log(chalk.yellowBright('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤСлава Україні! Слава Україні! Слава Україні! ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+        console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤV1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+
+    } else if (color === "lgbt") {
+        console.log(chalk.hex('#ff0000')("  ▄▄        ██████   ██████ ▓█████▄▄▄█████▓   ▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████    "))
+        console.log(chalk.hex('#FFA500')(" ▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒    "))
+        console.log(chalk.hex('#FFFF00')(" ▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███  ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄      "))
+        console.log(chalk.hex('#00FF00')(" ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄░ ▓██▓ ░    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒   "))
+        console.log(chalk.hex('#2E2EFF')("  ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒ ▒██▒ ░      ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒   "))
+        console.log(chalk.hex('#800080')(" ▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░        ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░  "))
+        console.log(chalk.hex('#800080')("   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░   ░           ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░   "))
+        console.log(chalk.hex('#800080')("   ░   ▒   ░  ░  ░  ░  ░  ░     ░    ░           ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░     "))
+        console.log(chalk.hex('#800080')("       ░  ░      ░        ░     ░  ░                        ░ ░      ░ ░      ░  ░      ░     "))
+        console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤV1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+    } else {
+        console.log("  ▄▄        ██████   ██████ ▓█████▄▄▄█████▓   ▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████    ")
+        console.log(" ▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒    ")
+        console.log(" ▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███  ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄      ")
+        console.log(" ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄░ ▓██▓ ░    ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒   ")
+        console.log("  ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒ ▒██▒ ░      ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒   ")
+        console.log(" ▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░        ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░  ")
+        console.log("   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░   ░           ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░   ")
+        console.log("   ░   ▒   ░  ░  ░  ░  ░  ░     ░    ░           ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░     ")
+        console.log("       ░  ░      ░        ░     ░  ░                        ░ ░      ░ ░      ░  ░      ░     ")
+        console.log(chalk.bgMagenta('ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤV1.0 Coded By: https://github.com/StiizzyCatㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ'))
+    }
+}
+
+module.exports = { theme }
